a86821ac21f078994336b6e5cb0d0b381e458b69
hyl-code <2839797603@qq.com>
2021-04-15 01:12:09 +0800

    up

---
+++ RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -0,0 +1,34 @@
+package com.example.roomwordssample;
+
+import android.content.Context;
+
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import static io.reactivex.internal.operators.single.SingleNever.INSTANCE;
+
+@Database(entities = {Word.class},version = 1,exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+    private static WordRoomDatabase INSTANCE;
+
+    static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    //Create database here
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),WordRoomDatabase.class,"word_database")
+                            //Wipes and rebuilds instead of migrating
+                            //if no Migration object.
+                            //Migration is not part of this practical
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}

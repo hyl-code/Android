0c341033cd0a43589cc1ce03da791674f410136f
hyl-code <2839797603@qq.com>
2021-04-16 09:52:56 +0800

    ++

--- RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
+++ RoomWordsSample/app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -1,11 +1,14 @@
 package com.example.roomwordssample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Dao;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import static io.reactivex.internal.operators.single.SingleNever.INSTANCE;
 
@@ -25,10 +28,45 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                             //if no Migration object.
                             //Migration is not part of this practical
                             .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+
+        @Override
+        public void onOpen (@NonNull SupportSQLiteDatabase db){
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    //Populate the database in the background.
+    private static class PopulateDbAsync extends AsyncTask<Void,Void,Void>{
+
+        private final WordDao mDao;
+        String[] words = {"dolphin","crocodile","cobra"};
+
+        PopulateDbAsync(WordRoomDatabase db){
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params){
+            //Start the app with a clean database every time.
+            //Not needed if you only populate the database
+            //when it is first created.
+            mDao.deleteAll();
+
+            for(int i = 0;i <= words.length - 1;i++){
+                Word word = new Word(words[i]);
+                mDao.insert(word);
+            }
+            return null;
+        }
     }
 }
